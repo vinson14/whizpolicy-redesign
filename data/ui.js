2fe67152985656a324643d632570aff450cd1376
Vinson Ong <vinson1493@gmail.com>
2021-12-02 18:29:56 +0800

    Changed button text from edit to update

--- data/ui.js
+++ data/ui.js
@@ -528,7 +528,7 @@ export const clientDetailsAboutFields = [
   {
     label: CLIENT_RELIGION_LABEL,
     key: CLIENT_RELIGION_KEY,
-    type: FIELD_TYPE_TEXT_CAMELCASE,
+    type: FIELD_TYPE_TEXT,
   },
 ];
 
