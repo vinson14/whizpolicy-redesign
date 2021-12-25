b96efac52e681d4e1cf6f3df658b4e68a0e61e6f
Vinson Ong <vinson1493@gmail.com>
2021-12-25 10:49:38 +0800

    Added per month for disability income

--- components/dashboard/client-details/coverage-info-popover.js
+++ components/dashboard/client-details/coverage-info-popover.js
@@ -1,5 +1,9 @@
 import { Grid, IconButton, Popover } from "@mui/material";
-import { CLIENT_ANNUAL_INCOME_KEY, coverageInfoPopoverContent } from "../../../data/ui";
+import {
+  CLIENT_ANNUAL_INCOME_KEY,
+  CLIENT_FINANCIAL_OVERVIEW_DISABILITY_INCOME_KEY,
+  coverageInfoPopoverContent,
+} from "../../../data/ui";
 import CloseIcon from "@mui/icons-material/Close";
 import { Fragment } from "react";
 import ClientCardInfoText from "../../stateless/interface/cards/client-card-info-text";
@@ -9,7 +13,7 @@ const CoverageInfoPopover = ({ anchorEl, handleClose, selectedKey, client }) => 
   const content = coverageInfoPopoverContent[selectedKey];
   const clientInfoText = [
     {
-      label: "Current Income",
+      label: "Estimated Annual Income",
       value: `$${formatNumber(client[CLIENT_ANNUAL_INCOME_KEY])}`,
     },
     {
@@ -42,7 +46,18 @@ const CoverageInfoPopover = ({ anchorEl, handleClose, selectedKey, client }) => 
           </Fragment>
         ))}
         {clientInfoText.map((text) => (
-          <ClientCardInfoText xs={12} sm={12} label={text.label} value={text.value} key={text.label} />
+          <ClientCardInfoText
+            xs={12}
+            sm={12}
+            label={text.label}
+            value={
+              (text.label === "Current Coverage" || text.label === "Ideal Coverage") &&
+              selectedKey === CLIENT_FINANCIAL_OVERVIEW_DISABILITY_INCOME_KEY
+                ? `${text.value}/month`
+                : text.value
+            }
+            key={text.label}
+          />
         ))}
       </Grid>
     </Popover>
